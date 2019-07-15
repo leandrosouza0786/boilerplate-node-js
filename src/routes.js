d80e02443777f61bd7d3b867644a6361adefd212
ljsouza <ljsouza@STFSAON003947-L>
2019-07-15 12:29:16 -0300

    require-dir add

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const routes = express.Router()
 
+const controller = require('./app/controllers')
+
+routes.get('/teste', controllers.index)
+
 module.exports = routes
